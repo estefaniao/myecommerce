7223bbef51a92e2f1e3c998c00fa7d89795ca641
esteotmo <esteotmo@suramericana.com.co>
2023-11-30 12:55:49 -0500

    Inicializar proyecto y conexion a la bd

---
+++ src/test/java/com/myecommerce/myecommerceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.myecommerce;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = { myecommerceApplicationTests.class })
+public class myecommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+}
